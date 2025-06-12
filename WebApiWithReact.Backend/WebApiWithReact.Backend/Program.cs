a215f2dc1167655a55e4e1f4c58afbca1ae9c05a
Sergey Avdeev <s.y.avdeev@gmail.com>
2025-06-12 15:52:19 +0700

    swagger added

--- WebApiWithReact.Backend/WebApiWithReact.Backend/Program.cs
+++ WebApiWithReact.Backend/WebApiWithReact.Backend/Program.cs
@@ -1,13 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSpaStaticFiles(options => options.RootPath = "wwwroot");
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllers();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.UseSpaStaticFiles();
 app.UseSpa(spaBuilder =>
 {
     spaBuilder.Options.SourcePath = "wwwroot";
 });
-app.MapControllers();
 
 app.Run();
